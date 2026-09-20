eee6fb8d23801e9a07ab46152ecdfaf4aeeb45b6
taimbaklouti <taimbaklouti@users.noreply.github.com>
2026-09-20 22:17:07 +0100

    fix: redirection updater/links vers taimbaklouti/gray-bongocat et bundle des modeles

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const GITHUB_LINK = 'https://github.com/ayangweb/BongoCat'
+export const GITHUB_LINK = 'https://github.com/taimbaklouti/Gray-BongoCat'
 
 export const UPGRADE_LINK_ACCESS_KEY = 'xDbrq2rOoRThDqKOHL2ZRA'
 
